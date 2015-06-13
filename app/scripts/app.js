9c289948d35c09a977eab3a07111c31c5af7645b
Alex Birch <birch@Alexs-MacBook-Pro.local>
2015-06-13 16:16:49 +0100

    Well that didn't work

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Home from './components/home';
 
+//console.log(require('modernizr'));
+console.log(require('easeljs'));
+
 window.React = React;
 const mountNode = document.getElementById('app');
 
--- app/scripts/components/key.js
+++ app/scripts/components/key.js
@@ -1,16 +1,28 @@
 import React from 'react';
 
+//console.log(require('easeljs'));
+
 export default class extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      canvasID: `${props.label}-canvas`
     };
+  }
+
+  componentDidMount() {
+    //var stage = new createjs.Stage(this.state.canvasID);
+    //var shape = new createjs.Shape();
+    //shape.graphics.beginFill('red').drawRect(0, 0, 120, 120);
+    //stage.addChild(shape);
+    //stage.update();
   }
 
   render() {
     return (
       <div>
-        <h3>I'm the {this.props.label} key</h3>
+        <h3>I'm the {this.props.label} key, brah yo</h3>
+        <canvas id={this.state.canvasID}></canvas>
       </div>
     );
   }
