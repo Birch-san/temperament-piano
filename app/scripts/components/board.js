8db3b8893a94ff4f2272279463e49baa071c8b12
Alex Birch <birch@Alexs-MacBook-Pro.local>
2015-06-14 20:48:45 +0100

    Finally found a useful explanation for major scales' existence

--- app/scripts/components/board.js
+++ app/scripts/components/board.js
@@ -3,30 +3,43 @@ import Key from './key';
 
 //import Trine from 'trine';
 import _ from 'lodash';
+// C2212221
+// A2122122
+// black keys are those which exist in the cycle of fifths,
+// but not within the keyboard's scale.
+
+// fifths can be (for example) 3:2 (i.e. perfect) or 5:4,
+// depending on how large you want your chromatic scale to be
+
+// natural notes are ones within scale
+
+// the natural notes in the major scale are the major tetrachords
+// of the tonic, dominant and subdominant!
+// http://arxiv.org/html/1202.4212v1/#sec_3_2_3
 
 export default class extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			seedLabel: "A",
-			nominalRange: 7,
-			scaleBase: "C"
+			scaleRoot: "A",
+			scale: [2,1,2,2,1,2,2],
+			scaleMode: "C"
 		};
 	}
 
 	render() {
-		let startCharCode = this.state.seedLabel.charCodeAt(0);
-		let keys=_(_.range(this.state.nominalRange))
+		let startCharCode = this.state.scaleRoot.charCodeAt(0);
+		let keys=_(_.range(this.state.scale.length))
 			.map(delta => {
 				return String.fromCharCode(startCharCode+delta)
 			})
 			.value();
 
-		let scaleBaseIndex = _.indexOf(keys, this.state.scaleBase);
-		let cycled = _.slice(keys, scaleBaseIndex).concat(_.slice(keys, 0, scaleBaseIndex));
+		let scaleBaseIndex = _.indexOf(keys, this.state.scaleMode);
+		let scaleModulusMode = _.slice(keys, scaleBaseIndex).concat(_.slice(keys, 0, scaleBaseIndex));
 
 		return (
-			<ul>{cycled.map(this.renderItem)}</ul>
+			<ul>{scaleModulusMode.map(this.renderItem)}</ul>
 		);
 	}
 
