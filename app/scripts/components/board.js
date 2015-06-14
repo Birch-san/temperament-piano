dada3f457d14e54f60a812759d8363b29ed181aa
Alex Birch <birch@Alexs-MacBook-Pro.local>
2015-06-14 16:10:52 +0100

    Prepare for trine

--- app/scripts/components/board.js
+++ app/scripts/components/board.js
@@ -3,22 +3,22 @@ import Key from './key';
 
 export default class extends React.Component {
   constructor(props) {
-    super(props);
-    this.state = {
-      keys: [
-        "C",
-        "G"
-      ]
-    };
+	super(props);
+	this.state = {
+	  keys: [
+          "C",
+          "G"
+	  ]
+	};
   }
 
   render() {
-    return (
-        <ul>{this.state.keys.map(this.renderItem)}</ul>
-    );
+	return (
+		<ul>{this.state.keys.map(this.renderItem)}</ul>
+	);
   }
 
   renderItem(item, index) {
-    return <li key={index}><Key label={item}>{item}</Key></li>;
+	return <li key={index}><Key label={item}>{item}</Key></li>;
   }
 }
