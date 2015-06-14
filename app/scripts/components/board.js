868d5715011a61b35c120bf6c78711ec10f87152
Alex Birch <birch@Alexs-MacBook-Pro.local>
2015-06-14 17:47:56 +0100

    Cyclical scale seeding

--- app/scripts/components/board.js
+++ app/scripts/components/board.js
@@ -1,24 +1,36 @@
 import React from 'react';
 import Key from './key';
 
+//import Trine from 'trine';
+import _ from 'lodash';
+
 export default class extends React.Component {
-  constructor(props) {
-	super(props);
-	this.state = {
-	  keys: [
-          "C",
-          "G"
-	  ]
-	};
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			seedLabel: "A",
+			nominalRange: 7,
+			scaleBase: "C"
+		};
+	}
 
-  render() {
-	return (
-		<ul>{this.state.keys.map(this.renderItem)}</ul>
-	);
-  }
+	render() {
+		let startCharCode = this.state.seedLabel.charCodeAt(0);
+		let keys=_(_.range(this.state.nominalRange))
+			.map(delta => {
+				return String.fromCharCode(startCharCode+delta)
+			})
+			.value();
 
-  renderItem(item, index) {
-	return <li key={index}><Key label={item}>{item}</Key></li>;
-  }
+		let scaleBaseIndex = _.indexOf(keys, this.state.scaleBase);
+		let cycled = _.slice(keys, scaleBaseIndex).concat(_.slice(keys, 0, scaleBaseIndex));
+
+		return (
+			<ul>{cycled.map(this.renderItem)}</ul>
+		);
+	}
+
+	renderItem(item, index) {
+		return <li key={index}><Key label={item}>{item}</Key></li>;
+	}
 }
